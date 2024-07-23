81ff955ec0c7286fadb72f2c34ee78db41484ac2
Alexey-sviridov <57225678+Alexey-sviridov@users.noreply.github.com>
2024-07-23 22:53:06 +0300

    Add files via upload

--- df_test.py
+++ df_test.py
@@ -4,8 +4,4 @@ import numpy as np
 
 uploaded_file = st.file_uploader("Choose a XLSX file", type="xlsx")
 
-if uploaded_file:
-    df = pd.read_excel(uploaded_file)
-
-    st.dataframe(df)
-    st.table(df)
+st.image("./for_decomp_example.xlsx")
