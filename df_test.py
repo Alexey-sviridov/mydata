48a6d4282c09baac82967884888147bc9b531179
Alexey-sviridov <57225678+Alexey-sviridov@users.noreply.github.com>
2024-07-23 22:56:17 +0300

    Add files via upload

--- df_test.py
+++ df_test.py
@@ -2,6 +2,6 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
-uploaded_file = st.file_uploader("Choose a XLSX file", type="xlsx")
+file = pd.read_excel("./for_decomp_example.xlsx")
 
-st.image("./for_decomp_example.xlsx")
+st.write('файл загружен')
